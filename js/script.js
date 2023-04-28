713aa23e0a93af8b1bc53b4bba94dd0b1a4f494d
ccoutob <rm97755@fiap.com.br>
2023-04-28 20:55:57 -0300

    Iniciando Javascript com operações e variaveis

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict"; //Não será mais possivel criar uma variavel sem valor atribuido.
+// console.log("Olá Mundo!!");
+
+// var nr1 = "10";
+// var nr2 = "5";
+
+// //Realizando oprerações com Strings e realizando o parse;
+// //função parseInt...;
+// console.log(parseInt(nr1) + parseInt(nr2));
+
+var nome1 = "Dubas";
+// let nome2 = "Serbato";
+// const nome3 = "Julia";
+
+if(nome1 != "") {
+    let nome1 = "Julia";//hoisnting
+}
+
+console.log(nome1);
