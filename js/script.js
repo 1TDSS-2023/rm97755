824cff3fe098cf52d000ac055027ccd77a9bdf62
ccoutob <rm97755@fiap.com.br>
2023-05-04 17:13:06 -0300

    Iniciando operações matemáticas

--- js/script.js
+++ js/script.js
@@ -19,3 +19,26 @@
 // var nome
 
 // console.log(nome1);
+
+/* <button value="click" id="meu-btn">Click</button> */
+const btn = document.getElementById("meu-btn");
+
+//Primeiro passo para a transformação em rgb...
+btn.addEventListener("click", function(){
+    // console.log(this.textContent);
+
+    //Função matemática math
+    //Random = retorna um número aleatório entre 0 e 1
+    //floor = Arredonda o numero pra baixo
+    //cell = Arredonda o numero para cima
+    //round = Arredonda número aleatório
+
+    let r=0,g=0,b=0;
+
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+    
+    
+
+} );
